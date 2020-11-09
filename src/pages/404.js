0470538487b131c923d0e892d588c6d24938bdbf
Dan6erbond <moravrav@gmail.com>
2020-11-09 16:59:51 +0100

    Update 404 page to original layout

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,24 @@
+import { Link } from "gatsby";
 import React from "react";
-
+import { Col, Container, Row } from "react-bootstrap";
+import { BsHouse } from "react-icons/bs";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Container>
+      <Row>
+        <Col className="text-center">
+          <h1>404: Not Found</h1>
+          <br />
+          <Link to="/" className="btn btn-outline-primary flat-button">
+            <BsHouse /> Return to Home
+          </Link>
+        </Col>
+      </Row>
+    </Container>
   </Layout>
 );
 
