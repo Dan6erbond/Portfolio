5af0f7df60b1bde2b94f35884ac0ab6d9c1268bd
Dan6erbond <moravrav@gmail.com>
2020-11-09 20:41:57 +0100

    [PAGE] Home Page, Use of Custom IsVisible Hook and Image Lazy Loading (#4)
    * Add cusotm `useIsVisible` hook to combat `react-vizibility-sensor` issues with Gatsby
    
    * Import Google fonts
    
    * Create code.jpg
    
    * Import scroll down button component from original CRA
    
    * Import skill bar component from CRA
    
    Using custom `useIsVisible` hook in place of react-vizibility-sensor.
    
    * Add homepage about component with text and skillbars
    
    Using `useIsVisible` hook for compatability.
    
    * Update site metadata
    
    * Import banner component from CRA and adjust styling for use with gatsby-image plugin
    
    * Fix profile image on home page
    
    * Import original home page with components and layout

---
+++ src/components/scrollDown.js
@@ -0,0 +1,32 @@
+import { motion } from "framer-motion";
+import PropTypes from "prop-types";
+import React from "react";
+import { FaArrowAltCircleDown } from "react-icons/fa";
+import "./scrollDown.scss";
+
+const ScrollDown = ({ targetRef, label }) => {
+  const scrollToRef = ref => window.scrollTo(0, ref.current.offsetTop);
+
+  return (
+    <div className="scroll-down">
+      <motion.button
+        onClick={() => scrollToRef(targetRef)}
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+      >
+        <FaArrowAltCircleDown />
+      </motion.button>
+      <span>{label}</span>
+    </div>
+  );
+};
+
+ScrollDown.propTypes = {
+  targetRef: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.shape({ current: PropTypes.elementType }),
+  ]),
+  label: PropTypes.string,
+};
+
+export default ScrollDown;
