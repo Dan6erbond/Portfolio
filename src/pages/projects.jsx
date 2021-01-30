b334edcb8489f23dc4aa74faf89fab4991ec58e8
Dan6erbond <moravrav@gmail.com>
2021-01-31 00:17:37 +0100

    Rename projects page

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
-const AboutPage = () => {
+const ProjectsPage = () => {
   return (
     <Layout>
       <SEO title="Home" />
@@ -10,4 +10,4 @@ const AboutPage = () => {
   );
 };
 
-export default AboutPage;
+export default ProjectsPage;
