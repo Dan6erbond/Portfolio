4ec137227acd39e784074a842a1b578f75e3e8ee
Dan6erbond <moravrav@gmail.com>
2021-01-30 23:35:50 +0100

    Eject from Gatsby TailwindCSS build process in production

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,6 +7,12 @@
 // You can delete this file if you're not using it
 import "./src/styles/global.css";
 
+if (process.env.NODE_ENV === "development") {
+  require("./src/styles/tailwind.css");
+} else {
+  require("./static/styles/tailwind.css");
+}
+
 export const wrapRootElement = ({ element }) => {
   if (typeof window !== "undefined") {
     const theme = localStorage.getItem("theme") || "dark";
