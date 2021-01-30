2369f4d40733d1236dd247b3e932686d45a6f3ae
Dan6erbond <moravrav@gmail.com>
2021-01-30 16:07:50 +0100

    Add SASS and TailwindCSS support

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,12 @@
+require("dotenv").config();
+
 module.exports = {
   siteMetadata: {
     title: "Portfolio",
   },
   plugins: [
     "gatsby-plugin-postcss",
+    "gatsby-plugin-sass",
     "gatsby-plugin-sharp",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
@@ -31,6 +34,21 @@ module.exports = {
         path: "./src/pages/",
       },
       __key: "pages",
+    },
+    {
+      resolve: "gatsby-plugin-firebase",
+      options: {
+        credentials: {
+          apiKey: "AIzaSyDrpc4GcICkLiTFmxkdUr_m3B7gHc3DLX0",
+          authDomain: "ravi-180905.firebaseapp.com",
+          databaseURL: "https://ravi-180905.firebaseio.com",
+          projectId: "ravi-180905",
+          storageBucket: "ravi-180905.appspot.com",
+          messagingSenderId: "276164819903",
+          appId: "1:276164819903:web:86d609df09653e6650bf0a",
+          measurementId: "G-2G5ETB38GK",
+        },
+      },
     },
   ],
 };
