f4b5b36efa6f6ad8e366ad3ca76e8feeb142cc8b
Dan6erbond <moravrav@gmail.com>
2021-01-30 16:39:25 +0100

    Add logo to config for icons and manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        icon: "src/images/icon.png",
+        icon: "src/images/logo_orange.svg",
       },
     },
     "gatsby-plugin-mdx",
