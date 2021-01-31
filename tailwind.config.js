fcafaa9ecc7019c7d25d73e4d1eadfdabbe1e175
Dan6erbond <moravrav@gmail.com>
2021-01-31 01:22:25 +0100

    FINALLY FIXED DARK MODE YIPEE!

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require("tailwindcss/colors");
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}"],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./gatsby-browser.js"],
   darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {
