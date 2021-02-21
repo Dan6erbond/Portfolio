774201b3f4886b50c06df25e1d8ba30d3812d1d3
Dan6erbond <moravrav@gmail.com>
2021-02-21 18:53:21 +0100

    Update blog.jsx

--- src/pages/blog.jsx
+++ src/pages/blog.jsx
@@ -20,15 +20,24 @@ const BlogPage = ({ data }) => {
           "mx-auto",
           "max-w-screen-lg",
         )}>
-        <div className={clsx("grid", "grid-cols-2", "gap-4")}>
+        <h1 className={clsx("text-3xl", "mb-4")}>My Blog</h1>
+        <p className={clsx("mb-8")}>View the blog posts I've written over the years.</p>
+        <div className={clsx("grid", "sm:grid-cols-2", "gap-4")}>
           {edges.map((edge) => (
             <div
               className={clsx("p-6", "bg-navy-600", "rounded-lg", "shadow-lg")}>
-              <p className={clsx("text-3xl", "mb-2", "leading-9")}>
+              <p
+                className={clsx(
+                  "text-2xl",
+                  "leading-8",
+                  "md:text-3xl",
+                  "mb-2",
+                  "leading-9",
+                )}>
                 {edge.node.frontmatter.title}
               </p>
               <p className={clsx("mb-4")}>{edge.node.frontmatter.date}</p>
-              <p className={clsx("mb-8", "text-gray-300")}>
+              <p className={clsx("mb-8", "text-gray-400")}>
                 {edge.node.frontmatter.description}
               </p>
               <Link
